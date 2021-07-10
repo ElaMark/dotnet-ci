87ec4919b0d69c828fa7a3406e6048a3f4397f9a
Mark@Instructor <Mark@Instructor>
2021-07-10 17:25:26 +1000

    Yuk

---
+++ .github/workflows/Bad.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace dotnet_ci
+{
+    public class MyClass
+    {
+        public void MyMethod()
+        {
+            string str = "";
+            for (int i = 0; i < 10; i++)
+            {
+                str = str + i + "-";  // yuk
+            }
+        }
+    }
+}
